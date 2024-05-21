579fbe9ff1f3402f1c76317c3d3d9d45a2f21174
Irina Carpincic <iryna.vorotynska@gmail.com>
2024-05-21 22:04:06 +0200

    Create Home page and improve the presentation of the Admin site

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -17,29 +17,31 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# Use include() to add URLS from the catalog application and authentication system
+from django.urls import include
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-# Use include() to add paths from the catalog application
-from django.urls import include
 
 urlpatterns += [
     path('catalog/', include('catalog.urls')),
 ]
 
-# Use static() to add URL mapping to serve static files during development (only)
+
+# Use static() to add url mapping to serve static files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 # Add URL maps to redirect the base URL to our application
 from django.views.generic import RedirectView
 urlpatterns += [
-    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
-
 
